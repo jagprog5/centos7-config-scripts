29f27c84f5b95032a5b398273ae471e8de0cac2e
John Giorshev <jg@localhost.localhost>
2020-08-22 17:49:38 -0400

    Add keepass2

---
+++ README.md
@@ -0,0 +1,7 @@
+## A setup script
+
+- Makes firefox completely dark mode (and gets add-ons dark-reader & ublock-origin)
+- Sets the gnome theme & background (everything dark)
+- Adds shortcut ctrl+alt+t => open terminal
+- Installs vscode, tmux, docker
+- Installs keepass2, creates launcher icon
--- setup.sh
+++ setup.sh
@@ -3,25 +3,46 @@
 VER=`cat /etc/redhat-release`
 [[ $VER =~ ^"CentOS Linux release 7"* ]] || exit 1
 
-sudo sh -c "echo \"<html><body style=\"background-color:black\;\"></body></html>\" > /usr/share/doc/HTML/index.html"
+sudo sh -c "echo -e \"<html>\n<body style=\"background-color:black\;\"></body>\n</html>\" > /usr/share/doc/HTML/index.html"
 cd ~/.mozilla/firefox/
 cd $(ls | grep '\.default-default$')
-echo "user_pref(\"browser.in-content.dark-mode\", true);\
-user_pref(\"browser.display.background_color\", \" #1a1a1a\");\
-user_pref(\"browser.startup.page\", 3);" > user.js
+echo -e "user_pref(\"browser.in-content.dark-mode\", true);\nuser_pref(\"browser.display.background_color\", \" #1a1a1a\");\nuser_pref(\"browser.startup.page\", 3);" > user.js
 
-curl -LO https://addons.mozilla.org/firefox/downloads/file/3615260/dark_reader.xpi
 # I can't automatically install the add-ons:
 # https://blog.mozilla.org/addons/2019/10/31/firefox-to-discontinue-sideloaded-extensions/
 # Unfortunately a button press is required per add-on
-firefox -new-window dark_reader.xpi
-curl -LO https://addons.mozilla.org/firefox/downloads/file/3629683/ublock_origin.xpi
+curl -LO "https://addons.mozilla.org/firefox/downloads/file/3615260/dark_reader.xpi"
+curl -LO "https://addons.mozilla.org/firefox/downloads/file/3629683/ublock_origin.xpi"
+READERHASH=`sha256sum dark_reader.xpi`
+if echo "$READERHASH" | grep -q "6027a82e9133aabb4536f9f182d27307156d00c05e891623ded5107c28542e55  dark_reader.xpi"; then
+    firefox -new-window dark_reader.xpi
+else
+    echo "dark_reader hash didn't match!"
+    echo "$READERHASH"
+    rm -v *.xpi
+    exit 1
+fi
+read -n 1 -s -r -p "Press any key when done with previous firefox add-on prompt."
+echo ""
+BLOCKERHASH=`sha256sum ublock_origin.xpi`
+if echo "$BLOCKERHASH" | grep -q "e9d2fa95b5323cec0e56e439b995326203d13c2fc781927741270ea34d244d30  ublock_origin.xpi"; then
+    firefox -new-window ublock_origin.xpi
+else
+    echo "ublock hash didn't match!"
+    echo "$BLOCKERHASH"
+    rm -v *.xpi
+    exit 1
+fi
+read -n 1 -s -r -p "Press any key when done with previous firefox add-on prompt."
+echo ""
+rm -v *.xpi
 
 hostnamectl set-hostname old-boi
 
 gsettings set org.gnome.desktop.interface enable-animations false
 gsettings set org.gnome.desktop.interface gtk-theme HighContrastInverse
-gsettings set org.gnome.desktop.background picture-uri file:///usr/share/backgrounds/7lines-top.png
+gsettings set org.gnome.desktop.background primary-color '#0B000B'
+gsettings set org.gnome.desktop.background picture-options none
 gsettings set org.gnome.desktop.screensaver picture-uri file:///usr/share/backgrounds/7lines-top.png
 gsettings set org.gnome.desktop.background show-desktop-icons false
 gsettings set org.gnome.desktop.interface show-battery-percentage true
@@ -32,25 +53,33 @@ gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/or
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-terminal/ command 'gnome-terminal'
 gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-terminal/ binding '<Primary><Alt>t'
 
-# get vscode
 sudo yum update -y
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo yum install -y code
 
-# get tmux
 sudo yum install -y tmux
 echo "alias tm=\"tmux attach || tmux\"" >> ~/.bashrc
 source ~/.bashrc
 
-sudo yum install -y yum-utils
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo sh -c 'echo -e "[docker-ce-stable]\nbaseurl=https://download.docker.com/linux/centos/7/$basearch/stable\nenabled=1\ngpgcheck=1\ngpgkey=https://download.docker.com/linux/centos/gpg" > /etc/yum.repos.d/docker-ce.repo'
 sudo yum install -y docker-ce docker-ce-cli containerd.io
 sudo systemctl enable docker.service
 
-read -n 1 -s -r -p "Press any key when done with previous firefox add-on prompt."
-echo ""
-firefox -new-window ublock_origin.xpi
-read -n 1 -s -r -p "Press any key when done with previous firefox add-on prompt."
-echo ""
-rm -v *.xpi
+# keepass2
+sudo sh -c 'echo -e "[mono-centos7-stable]\nbaseurl=https://download.mono-project.com/repo/centos7-stable/\nenabled=1\ngpgcheck=1\ngpgkey=https://download.mono-project.com/repo/xamarin.gpg" > /etc/yum.repos.d/mono-centos7-stable.repo'
+sudo yum install -y mono
+curl -o keepass2.zip -L "https://sourceforge.net/projects/keepass/files/KeePass 2.x/2.45/KeePass-2.45.zip/download"
+KEEPASSHASH=`sha256sum keepass2.zip`
+if echo "$KEEPASSHASH" | grep -q "d414db9d411acbafd4e1b5faa8605ad7a28829f1814ce779142ffbd5ca9c3794  keepass2.zip"; then
+    sudo unzip keepass2.zip -d /opt/keepass2
+    sudo curl "https://upload.wikimedia.org/wikipedia/commons/0/04/KeePass_icon.svg" -o /opt/keepass2/icon.svg
+    sudo sh -c 'echo -e "[Desktop Entry]\nType=Application\nTerminal=false\nName=KeePass2\nIcon=/opt/keepass2/icon.svg\nExec=mono /opt/keepass2/KeePass.exe" > /usr/share/applications/keepass2.desktop'
+    sudo update-desktop-database
+else
+    echo "keepass2 hash didn't match!"
+    echo "$KEEPASSHASH"
+    rm keepass2.zip
+    exit 1
+fi
+rm keepass2.zip
+
