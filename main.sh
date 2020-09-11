be9ed5517253776dff8337f5d48e20decc4cc33f
John Giorshev <John Giorshev>
2020-09-11 18:42:56 -0400

    Fix tmux install

--- main.sh
+++ main.sh
@@ -3,7 +3,7 @@ set -o pipefail
 
 VER=`cat /etc/redhat-release`
 [[ $VER =~ ^"CentOS Linux release 7"* ]] || {
-    echo "Wrong os version: $VER"
+    echo "Wrong os: $VER"
     exit 1
 }
 
@@ -34,16 +34,19 @@ confirm_prompt() {
     done
 }
 
-confirm_prompt "Install vscode?" && cat "$DIR"/files/vscode | sudo dd of=/etc/yum.repos.d/vscode.repo 2>/dev/null &&
+confirm_prompt "Install vscode?" && sudo sh -c "cat \"$DIR\"/files/vscode > /etc/yum.repos.d/vscode.repo" &&
     sudo yum install -y code
-confirm_prompt "Install docker?" && cat "$DIR"/files/docker | sudo dd of=/etc/yum.repos.d/docker-ce.repo 2>/dev/null &&
+confirm_prompt "Install docker?" && sudo sh -c "cat \"$DIR\"/files/docker > /etc/yum.repos.d/docker-ce.repo" &&
     sudo yum install -y docker-ce docker-ce-cli containerd.io &&
     sudo systemctl enable docker.service
-confirm_prompt "Install keepass2, and mono?" && cat "$DIR"/files/mono | sudo dd of=/etc/yum.repos.d/mono-centos7-stable.repo 2>/dev/null &&
+confirm_prompt "Install keepass2, and mono?" && sudo sh -c "cat \"$DIR\"/files/mono > /etc/yum.repos.d/mono-centos7-stable.repo" &&
+    (
+        test -d /opt/keepass2 && sudo rm -r /opt/keepass2
+    ) &&
     sudo yum install -y mono &&
     curl -o keepass2.zip -L "https://sourceforge.net/projects/keepass/files/KeePass 2.x/2.45/KeePass-2.45.zip/download" && 
     (
-        if verify_hash ad414db9d411acbafd4e1b5faa8605ad7a28829f1814ce779142ffbd5ca9c3794 keepass2.zip
+        if verify_hash d414db9d411acbafd4e1b5faa8605ad7a28829f1814ce779142ffbd5ca9c3794 keepass2.zip
         then
             exit 0
         else
@@ -53,7 +56,7 @@ confirm_prompt "Install keepass2, and mono?" && cat "$DIR"/files/mono | sudo dd 
     ) &&
     sudo unzip keepass2.zip -d /opt/keepass2 &&
     sudo curl "https://upload.wikimedia.org/wikipedia/commons/0/04/KeePass_icon.svg" -o /opt/keepass2/icon.svg &&
-    cat "$DIR"/files/keepass2 | sudo dd of=/usr/share/applications/keepass2.desktop 2>/dev/null &&
+    sudo sh -c "cat \"$DIR\"/files/keepass2 > /usr/share/applications/keepass2.desktop" &&
     sudo update-desktop-database &&
     rm keepass2.zip
 confirm_prompt "Build and install git 2.28.0?" &&
@@ -98,7 +101,7 @@ confirm_prompt "Set gnome theme? " && {
     gsettings set org.gnome.desktop.interface show-battery-percentage true
     gsettings set org.gnome.desktop.interface clock-show-date true
 }
-confirm_prompt "Set ctrl+alt+t => open terminal? Warning: overwrites existing shortcuts." && {
+confirm_prompt "Set shortcuts? Warning: overwrites existing shortcuts." && {
     gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-terminal/']"
     gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-terminal/ name 'Open Terminal'
     gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/open-terminal/ command 'gnome-terminal'
@@ -127,12 +130,15 @@ confirm_prompt "Build and install tmux 2.9?" &&
     cd .. &&
     sudo rm -r tmux-2.9 && 
     cat "$DIR"/files/tmux > ~/.tmux.conf &&
-    echo "alias tm=\"tmux attach || tmux\"" >> ~/.bashrc &&
-    source ~/.bashrc && 
-    sudo yum remove -y libevent-devel ncurses-devel automake &&
-
-
-confirm_prompt "Configure firefox?" && 
+    (
+        cat ~/.bashrc | grep -q "alias tm=\"tmux attach || tmux\""
+        if test $? -ne 0
+        then
+            echo "alias tm=\"tmux attach || tmux\"" >> ~/.bashrc
+            source ~/.bashrc
+        fi
+    ) &&
+    sudo yum remove -y libevent-devel ncurses-devel automake
 
 echo "Enter hostname: "
 read HOST_NAME
